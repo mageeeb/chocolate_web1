f115b9b9c6e24e2cee928e7caa319b868d1d963e
mikhawa <michaeljpitz@gmail.com>
2025-11-24 11:18:44 +0100

    test main

---
+++ temporaire_transition/test/CommentTestEntity.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+spl_autoload_register(function ($class) {
+    $class = str_replace('\\', '/', $class);
+    require '../src/' .$class . '.php';
+});
+
+use model\mapping\CommentMapping;
+
+$testComment = new CommentMapping([
+    ]);
+
+$testComment1 = new CommentMapping([
+    'id' => 1,
+    'content' => 'This is a test comment.',
+    'created_at' => new DateTimeImmutable('2024-06-01 12:00:00'),
+    'is_accepted' => 1
+]);
+
+var_dump($testComment,$testComment1);
+
+
